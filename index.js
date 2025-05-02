0188933d1c22863c2b998c11945aa1f6e3beec80
Yashwant Kumar <Yashwantkumar521@gmail.com>
2025-05-02 12:10:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ client.on('ready', async () => {
 client.on('interactionCreate', async interaction => {
   if (!interaction.isChatInputCommand()) return;
   if (interaction.commandName === 'myhours') {
-    if (interaction.commandName === 'myhours') {
     const data = await StudyTime.findOne({ userId: interaction.user.id });
     const cameraOnHours = data ? Math.floor(data.cameraOn / 3600) : 0;
     const cameraOffHours = data ? Math.floor(data.cameraOff / 3600) : 0;
