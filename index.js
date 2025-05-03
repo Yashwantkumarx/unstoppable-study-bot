1a31cd360f445391658ae3d1a22ad85265852a52
Yashwant Kumar <Yashwantkumar521@gmail.com>
2025-05-03 10:50:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -137,18 +137,11 @@ if (commandName === 'myhours') {
 
   return interaction.reply(
     `**✨ Hey _${targetUser.username}_! Here's your Study Report
-    
-   📆 ${currentDate}:**\n` +
-    
-    
+      📆 ${currentDate}:**\n` +    
     `**📷 Camera On:** **${data.camOn.toFixed(2)} hrs ✅** — _${focusTag}_\n` +
     `**📷 Camera Off:** **${data.camOff.toFixed(2)} hrs ❌** — _${silentTag}_\n` +
-    
-    
-    `**🕒 Total Time:** **${total.toFixed(2)} hrs**\n` +
-    
-    
-    `**⚡ Keep going, Champion! You're unstoppable!**`
+        `**🕒 Total Time:** **${total.toFixed(2)} hrs**\n` +
+        `**⚡ Keep going, Champion! You're unstoppable!**`
   );
 }
 
